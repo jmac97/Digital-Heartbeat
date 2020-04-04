2e26304f308cba605b56e5dbf2823c8112931cb7
JSM <47127982+jmac97@users.noreply.github.com>
2020-04-04 18:14:37 -0400

    Delete heart.ino

--- heart.ino
+++
@@ -1,9 +0,0 @@
-//                                                                                                                                                                                                                                                                                                                                                                                                                         #include "beat.cpp"
-Beat heart(81, 5);
-
-void setup() {
-}
-
-void loop() {
-  heart.lubDub();
-}
