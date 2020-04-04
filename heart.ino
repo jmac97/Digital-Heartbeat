8cfc103c3cc99a8d4a79e2ac94bb8865112fb454
JSM <47127982+jmac97@users.noreply.github.com>
2020-04-04 18:12:47 -0400

    Add files via upload

---
+++ heart.ino
@@ -0,0 +1,9 @@
+//                                                                                                                                                                                                                                                                                                                                                                                                                         #include "beat.cpp"
+Beat heart(81, 5);
+
+void setup() {
+}
+
+void loop() {
+  heart.lubDub();
+}
